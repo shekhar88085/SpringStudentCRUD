22fe4a930ff23f5e17e5b03bd1ba9d38acc82da0
deraganotra <deraganotra@deraganotra-HP-Notebook>
2021-06-19 12:17:19 +0530

    API layer BUSINESS layer DI

--- SpringStudentCRUD/src/main/java/com/example/demo/SpringStudentCrudApplication.java
+++ SpringStudentCRUD/src/main/java/com/example/demo/SpringStudentCrudApplication.java
@@ -1,7 +1,18 @@
 package com.example.demo;
+//import com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
+
+//import com.sun.tools.javac.util.List;
 
 @SpringBootApplication
 public class SpringStudentCrudApplication {
@@ -9,5 +20,6 @@ public class SpringStudentCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudentCrudApplication.class, args);
 	}
+	
 
 }
